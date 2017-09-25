b7eca38daa1267709391936bfd5cc9e7900d1096
ZhangYang <942398220@qq.com>
2017-09-25 23:03:29 +0800

    kotlin配置修改

--- app/src/main/java/com/zydemos/simplerefresh/MainActivity.kt
+++ app/src/main/java/com/zydemos/simplerefresh/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
+
                 mTextMessage!!.setText(R.string.title_home)
                 return@OnNavigationItemSelectedListener true
             }
