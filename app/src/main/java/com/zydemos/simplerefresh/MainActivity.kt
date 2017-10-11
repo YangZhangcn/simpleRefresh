7f3c4fa1c09e1c8c02e3ef62f434dd1d49b13279
ZhangYang <942398220@qq.com>
2017-10-11 21:29:53 +0800

    kotlin配置修改

--- app/src/main/java/com/zydemos/simplerefresh/MainActivity.kt
+++ app/src/main/java/com/zydemos/simplerefresh/MainActivity.kt
@@ -9,7 +9,6 @@ import android.widget.TextView
 class MainActivity : AppCompatActivity() {
 
     private var mTextMessage: TextView? = null
-
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
